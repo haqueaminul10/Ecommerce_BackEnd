1dcb330249858b8565feddd90896b61058fdf4a0
haqueaminul10 <haqueaminul10100@gmail.com>
2024-12-03 16:56:28 +0600

    setup server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('setting up server');
+// REQUIRE EXPRESS
+const express = require('express');
+const app = express();
+app.use(express.json()); // Parse JSON payloads
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded payloads
+
+//DOTENV
+const dotenv = require('dotenv');
+dotenv.config();
+
+// PORT
+const PORT = process.env.PORT;
+
+//SERVER LISTEN
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log(`📌 ~ app.listen ~ error:`, error);
+  } else {
+    console.log(`server is running ${PORT}`);
+  }
+});
